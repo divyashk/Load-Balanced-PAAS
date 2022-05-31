20f39913965f3d17370023b7bf2cd82da34f92b8
Signior-X <b19188@students.iitmandi.ac.in>
2022-05-31 23:29:55 +0530

    CSS fixed and terminal working

--- machines/app.py
+++ machines/app.py
@@ -1,5 +1,5 @@
 from time import sleep
-from flask import Flask, render_template, jsonify, request , redirect , url_for, session
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 from werkzeug.utils import secure_filename
 import docker
 from zipfile import ZipFile
@@ -11,8 +11,9 @@ from flask_socketio import SocketIO, emit
 
 
 UPLOAD_FOLDER = 'zipfiles'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif' , 'zip'}
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip'}
 CONNECTED_CLIENTS = []
+ADMIN_PASSWORD = "admin"
 
 
 client = docker.from_env()
@@ -26,16 +27,30 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 def get_freeport():
     freeport = ""
     with socket() as s:
-        s.bind(('',0))
+        s.bind(('', 0))
         freeport = s.getsockname()[1]
     return freeport
 
+
 @app.route('/')
 def index_page():
-    return render_template("index.html")
+    return "Machine Home!"
+
+
+@app.route('/term')
+def term_page():
+    ''' Pass the Admin password for the service '''
+    if "password" not in request.args:
+        return jsonify(success=False, error="password not provided!")
+
+    if request.args["password"] == ADMIN_PASSWORD:
+        return render_template('term.html')
+
+    return jsonify(success=False, error="Wrong Password!")
 
 
 @app.route('/stats', methods=["GET", "POST"])
@@ -52,7 +67,16 @@ def run_cmd_on_machine():
     if cmd is None:
         return jsonify(success=False, error="No command(cmd) provided")
 
-    output = os.popen(cmd).read()
+    output = ""
+    cwd = os.getcwd()
+    if "cwd" in request.json:
+        cdd = request.json["cwd"]
+        if cdd != "":
+            os.chdir(cdd)
+        output = os.popen(cmd).read()
+        os.chdir(cwd)
+    else:
+        output = os.popen(cmd).read()
     print(output)
 
     return jsonify(success=True, output=output)
@@ -65,40 +89,44 @@ def build_from_hub():
     print(repo)
     image = client.images.pull(repo)
     freeport = get_freeport()
-    container = client.containers.run(repo + ':latest' , ports = {5000 : freeport} , detach=True)
+    container = client.containers.run(
+        repo + ':latest', ports={5000: freeport}, detach=True)
     print(container)
     print(freeport)
     return jsonify(success=True, port=freeport)
+
 
 @app.route('/build-from-zip', methods=['POST'])
 def build_from_zip():
     # add current user to group docker, and also run "newgrp docker"
     if 'file' not in request.files:
-        return {'message' : 'File not sent'}
+        return {'message': 'File not sent'}
     file = request.files['file']
     if file.filename == '':
-        return {'message' : 'File not sent'}
+        return {'message': 'File not sent'}
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'] , filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         appname = request.form["appname"]
         print(filepath)
         print(appname)
-        with ZipFile(filepath , 'r') as zipObj:
+        with ZipFile(filepath, 'r') as zipObj:
             zipObj.extractall('extracted_zips/' + appname)
-            zipImage = client.images.build(path = 'extracted_zips/' + appname)[0]
-            zipImage.tag(appname , "latest")
+            zipImage = client.images.build(path='extracted_zips/' + appname)[0]
+            zipImage.tag(appname, "latest")
             print(client.images.list())
         freeport = get_freeport()
         print(freeport)
         # container = client.containers.run(appname + ":latest" , ports = {5000 : freeport} , detach=True)
-        container = client.containers.run(appname + ":latest", ports = {5000 : freeport} , detach=True)
-        print(container.id , container.labels , container.image)
+        container = client.containers.run(
+            appname + ":latest", ports={5000: freeport}, detach=True)
+        print(container.id, container.labels, container.image)
         print(client.containers.list())
         return jsonify(success=True, port=freeport)
     else:
         return jsonify(success=False)
+
 
 app.run(port=8080, host="0.0.0.0")
